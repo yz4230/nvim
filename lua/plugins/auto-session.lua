1cfe3540d2ce14865201f865b6c17bd3b789bb23
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2026-03-11 05:50:38 +0000

    plugin: remove auto-session

--- lua/plugins/auto-session.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rmagatti/auto-session",
-  config = function()
-    require("auto-session").setup {
-      pre_save_cmds = { "Neotree close" },
-      post_restore_cmds = { "Neotree filesystem show" },
-    }
-  end,
-}
