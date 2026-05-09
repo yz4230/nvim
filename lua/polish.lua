ca63693acb61a727d5978c795f3cdb4e47e62158
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2026-05-10 00:14:10 +0900

    feat: disable clipboard sync when D or X key is pressed

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,10 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({ "n", "x" }, "x", '"_x', opts)
+vim.keymap.set({ "n", "x" }, "X", '"_X', opts)
+vim.keymap.set({ "n", "x" }, "d", '"_d', opts)
+vim.keymap.set({ "n", "x" }, "D", '"_D', opts)
