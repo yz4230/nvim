be4f6d1eb470566348fc4df3bb33d219c6ab2194
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2026-05-11 01:03:15 +0900

    plugin: add toml

--- lua/community.lua
+++ lua/community.lua
@@ -14,6 +14,7 @@ return {
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.yaml" },
