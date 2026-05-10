4ef6c750beb4f67a7663dce5f6f62a878928b6b6
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2026-05-11 01:18:20 +0900

    fix: mise.toml support

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -9,13 +9,7 @@ return {
           evenBetterToml = {
             schema = {
               associations = {
-                ["https://mise.jdx.dev/schema/mise.json"] = {
-                  "mise.toml",
-                  ".mise.toml",
-                  "**/mise.toml",
-                  "**/.mise.toml",
-                  vim.fn.expand "~/.config/mise/config.toml",
-                },
+                ["mise\\.toml$"] = "https://mise.jdx.dev/schema/mise.json",
               },
             },
           },
